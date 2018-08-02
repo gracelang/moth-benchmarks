5a29585c0f38619537163f1546a2eb545f15324d
Richard Roberts <richardandrewroberts@gmail.com>
2018-08-02 19:21:33 +1200

    Setup benchmark execution
    - new executable
    - add benchmark configuration (codespeed)
    - implement CI/CD
    - ignore benchmark results

---
+++ benchmark.sh
@@ -0,0 +1,29 @@
+# Top-level executable for "Are We Fast Yet" benchmarks
+#   Originally developed by Stefan Marr
+#   https://gitlab.soft.vub.ac.be/stefan.marr/awfy-runs
+#
+# Adapted by Richard Roberts
+#
+
+# #!/bin/bash
+PARAMS=("-d" "-c" "--commit-id=$CI_BUILD_REF" "--environment=yuria" "--branch=$CI_BUILD_REF_NAME") 
+rebench -f -c "${PARAMS[@]}" codespeed.conf all
+
+
+## Archive results
+
+DATA_ROOT=~/benchmark-results/moth-benchmarks
+
+REV=`git rev-parse HEAD | cut -c1-8`
+
+NUM_PREV=`ls -l $DATA_ROOT | grep ^d | wc -l`
+NUM_PREV=`printf "%03d" $NUM_PREV`
+
+TARGET_PATH=$DATA_ROOT/$NUM_PREV-$REV
+LATEST=$DATA_ROOT/latest
+
+mkdir -p $TARGET_PATH
+cp benchmark.data $TARGET_PATH/
+rm $LATEST
+ln -s $TARGET_PATH $LATEST
+echo Data archived to $TARGET_PATH
