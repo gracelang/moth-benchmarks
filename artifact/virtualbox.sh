94ccde9d541fa4775e16229c860b9fa34204e0c1
Stefan Marr <git@stefan-marr.de>
2019-04-09 02:42:33 +0100

    Added adapted artifact setup from ManLang18
    - allow faulty runs to ignore Higgs timeout
    - build eval-description
    - install Latex
    - use full desktop Ubuntu
    - give more resources to VM
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ artifact/virtualbox.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e # make script fail on first error
+
+# make SCRIPT_PATH absolute
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
+source ${SCRIPT_PATH}/../implementations/script.inc
+source ${SCRIPT_PATH}/config.inc
+
+## Make sure we have all dependencies
+check_for "vagrant" "Vagrant does not seem available. This was tested with version 2.1.2 from https://www.vagrantup.com/downloads.html"
+
+INFO Setup a Vagrant VM Image
+pushd ${SCRIPT_PATH}
+
+INFO "Get Paper Repository for Evaluation Material"
+git clone --depth=1 ${PAPER_REPO} paper
+
+vagrant up
+vagrant halt
+vagrant package --output ${BOX_NAME}.box
+popd
