ca2fac41588b8a5d3ccbca17893849f16c1ef366
Stefan Marr <git@stefan-marr.de>
2019-05-09 16:51:49 +0100

    Added cleanup for GraalBasic compilation to safe space
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- artifact/build.sh
+++ artifact/build.sh
@@ -9,6 +9,15 @@ cd GraalBasic
 git checkout d37bbe4de590087231cb17fb8e5e08153cd67a59
 
 ./build.sh
+
+## Clean Graal Build Folder, not needed
+export JVMCI_VERSION_CHECK=ignore
+export JAVA_HOME=~/.local/graal-core
+#/usr/lib/jvm/java-8-openjdk-amd64/
+(cd graal-jvmci-8;    ../mx/mx clean)
+(cd truffle/compiler; ../../mx/mx clean)
+(cd truffle/sdk;      ../../mx/mx clean)
+
 cd ..
 export GRAAL_HOME=~/.local/graal-core
 echo "" >> ~/.profile
